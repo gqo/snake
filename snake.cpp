106e1d84fed71733b25270398f8155b4dd34383f
Graeme <ggmferguson@gmail.com>
2017-10-11 12:56:15 -0400

    added comments and changed style from SFML boilerplate

--- snake.cpp
+++ snake.cpp
@@ -2,27 +2,27 @@
 
 int main()
 {
+    // Initialize popup window, i.e. the game window
     sf::RenderWindow window(sf::VideoMode(500, 500), "Snake");
-    // Initialize background shape
+    // Initialize background shape and look
     sf::RectangleShape background(sf::Vector2f(480,480));
     background.setFillColor(sf::Color::White);
     background.setOutlineThickness(10);
     background.setOutlineColor(sf::Color::Red);
     background.setPosition(10,10);
-
+    // Creates game window instance
     while (window.isOpen())
     {
+        // Tracks interactions with window
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            // Checks if window was closed and then closes window
+            if (event.type == sf::Event::Closed) { window.close(); }
         }
-
         window.clear();
         window.draw(background);
         window.display();
     }
-
     return 0;
 }
